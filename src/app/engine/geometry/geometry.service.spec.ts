e7bef47955b75f9b74ec3a58701613f17cbff630
Conor Kingston <cmkingston21@me.com>
2020-09-15 10:24:12 -0700

    added methods toSpherical and toCartesian; added unit tests

--- src/app/engine/geometry/geometry.service.spec.ts
+++ src/app/engine/geometry/geometry.service.spec.ts
@@ -13,4 +13,24 @@ describe('GeometryService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('should convert CARTESIAN to SPHERICAL coordinates', () => {
+    let spherical, cartesian;
+
+    it('should convert [ 0, 0, 0 ]', () => {
+      spherical = service.toSpherical( [ 0, 0, 0 ] ).coordinates;
+      cartesian = [ 0, 0, 0 ];
+      expect(spherical).toEqual(cartesian);
+    });
+  });
+
+  describe('should convert SPHERICAL to CARTESIAN coordinates', () => {
+    let spherical, cartesian;
+
+    it('should convert [ 0, 0, 0 ]', () => {
+      cartesian = service.toCartesian( [ 0, 0, 0 ] ).coordinates;
+      spherical = [ 0, 0, 0 ];
+      expect(cartesian).toEqual(spherical);
+    })
+  })
 });
--- src/app/engine/geometry/geometry.service.ts
+++ src/app/engine/geometry/geometry.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Coordinate, TripleNumArray, CartesianTuple } from 'coordinate-systems';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeometryService {
+  public xyz: Coordinate;
+  public spherical: Coordinate; // { radius, theta, phi }
 
   constructor() { }
+
+  public toSpherical( coordinates: CartesianTuple ): Coordinate {
+    // FROM cartesian
+      // TO spherical
+
+    this.xyz = Coordinate.cartesian( coordinates );
+
+    return this.xyz;
+  }
+
+  public toCartesian( coordinates: TripleNumArray ): Coordinate {
+    // FROM spherical
+      // TO cartesian
+    
+    this.spherical = Coordinate.spherical( coordinates );
+    
+    return this.spherical;
+  }
 }
